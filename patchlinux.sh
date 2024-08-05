72a30eb2d828b668c570403ed63d58db96427588
prasanna <149587787+prasannanjaneyreddychalla@users.noreply.github.com>
2024-08-05 20:24:23 +0530

    second commit

--- patchlinux.sh
+++ patchlinux.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 #check the Linux Distribution and proceed only for fedora family systems
-if ! grep -E "CentOS|centos|rhel|fedora|rocky" /etc/*-release ; then
+if ! grep -E "CentOS|centos|rhel|fedora|rocky" /etc/*-release >/dev/null ; then
     echo "Not a supported linux distribution, the current supported systems are rhel based"
     exit 1
 fi
@@ -14,7 +14,7 @@ trb1() {
     mv /var/lib/rpm/__db*  /tmp
     yum clean all
     rpm --rebuilddb
-    yum update --security --bugfix --exclude=java* -y 
+    #yum update --security --bugfix --exclude=java* -y 
     rm -rf /tmp/__db*
 }
 
@@ -22,13 +22,12 @@ trb2() {
     # Reconfiguring repo if there's an issue with older ones or if they aren't enabled at all 
     echo "Reconfiguring repos.."
     mv /etc/sysconfig/rhn /etc/sysconfig/rhnold
-    cd /etc/yum.repos.d || exit 1
     # NOTE : yum.repos.d directory will be blank in most of the server but in some server it will have a file called "spacewalk-bootstrap.repo" , which can be deleted )
-    rm -f *
-    wget http://enghost.prod.ch3.s.com/~syseng/repo_aws/CentOS-Base.repo
-    wget http://enghost.prod.ch3.s.com/~syseng/repo_aws/CentOS-CR.repo
-    wget http://enghost.prod.ch3.s.com/~syseng/repo_aws/CentOS-Debuginfo.repo
-    wget http://enghost.prod.ch3.s.com/~syseng/repo_aws/epel.repo
+    rm -f /etc/yum.repos.d/*
+    wget -P /etc/yum/repos.d/ http://enghost.prod.ch3.s.com/~syseng/repo_aws/CentOS-Base.repo
+    wget -P /etc/yum/repos.d/ http://enghost.prod.ch3.s.com/~syseng/repo_aws/CentOS-CR.repo
+    wget -P /etc/yum.repos.d/ http://enghost.prod.ch3.s.com/~syseng/repo_aws/CentOS-Debuginfo.repo
+    wget -P /etc/yum.repos.d/ http://enghost.prod.ch3.s.com/~syseng/repo_aws/epel.repo
     yum clean all
     yum repolist
     puppet agent --disable
@@ -43,13 +42,13 @@ patchcommand1() {
 }
 
 patchcommand2() {
-    # We use these repos in few systems
+    # We use these repos in few centos systems
     yum update --security --bugfix --exclude=java* --disablerepo=* --enablerepo=CentOS-Base,CentOS-CR,CentOS-Debuginfo,epel -y
 }
 
 
 # Run the command and store the output into a variable
-output_log=$patchcommand1
+output_log=$patchcommand2
 
 # Check if the update is successful
 if [ $? -eq 0 ]; then
